32fd12e2b75f756eb9ad05ba8ad384509c6a1a3d
barbara m avakian <bzavakian@charter.net>
2013-03-14 12:39:31 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punkrocktacos::Application.config.secret_token = '26e868faf119e25ed82ebcf3d900c4115ed21e786f96ab022ac7c09e5e1e7de158407ad7fafbcbe0a3a70dd15b3607fb32c45e03539e5ee6785027c2072ce69b'
